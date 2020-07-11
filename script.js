a3e79820c1d707461e8cbc34147a24d051bce86d
leovdn <leo_vdn@hotmail.com>
2020-07-11 20:06:24 -0300

    testing DOM elements manipulation for updating data

--- script.js
+++ script.js
@@ -3,8 +3,15 @@ import Countdown from './countdown.js';
 const TempoAteNatal = new Countdown('25 December 2020 23:59:59 GMT-0300');
 const TempoAteAnoNovo = new Countdown('31 December 2020 23:59:59 GMT-0300');
 
+const textElement = document.querySelector('p');
+
+
 
 setInterval(() => {
-    console.log(TempoAteNatal.total)
-
-}, 10000)
+    textElement.innerHTML = 
+        `   Dias: <h2>${TempoAteAnoNovo.total.days}</h2>
+            Horas: <h2>${TempoAteAnoNovo.total.hours}</h2>
+            Minutos: <h2>${TempoAteAnoNovo.total.minutes}</h2>
+            Segundos: <h2>${TempoAteAnoNovo.total.seconds}</h2>
+        `;
+}, 1000)
