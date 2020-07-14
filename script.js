c5906351ff0009dfdb44a5f6faaa987978271dcf
leovdn <leo_vdn@hotmail.com>
2020-07-13 21:35:58 -0300

    created functions for card classes variables

--- script.js
+++ script.js
@@ -2,21 +2,54 @@ import Countdown from './countdown.js';
 
 const TempoAteNatal = new Countdown('25 December 2020 23:59:59 GMT-0300');
 const TempoAteAnoNovo = new Countdown('31 December 2020 23:59:59 GMT-0300');
-
-const daysValue = document.querySelector('.days');
-const hoursValue = document.querySelector('.hours');
-const minutesValue = document.querySelector('.minutes');
-const secondsValue = document.querySelector('.seconds');
-
-setInterval(() => {
-    daysValue.innerHTML = `${TempoAteNatal.total.days}`;
-    hoursValue.innerHTML = `${TempoAteNatal.total.hours}`;
-    minutesValue.innerHTML = `${TempoAteNatal.total.minutes}`;
-    secondsValue.innerHTML = `${TempoAteNatal.total.seconds}`;
-}, 1000)
+const TempoAteCarnaval = new Countdown('16 February 2021 23:59:59 GMT-0300');
 
 
+function natalCount() {
+    const daysValue = document.querySelector('.natal .days');
+    const hoursValue = document.querySelector('.natal .hours');
+    const minutesValue = document.querySelector('.natal .minutes');
+    const secondsValue = document.querySelector('.natal .seconds');
+
+    setInterval(() => {
+        daysValue.innerHTML = `${TempoAteNatal.total.days}`;
+        hoursValue.innerHTML = `${TempoAteNatal.total.hours}`;
+        minutesValue.innerHTML = `${TempoAteNatal.total.minutes}`;
+        secondsValue.innerHTML = `${TempoAteNatal.total.seconds}`;
+    }, 1000);
+};
+
+natalCount();
 
 
+function anoNovoCount() {
+    const daysValue = document.querySelector('.ano-novo .days');
+    const hoursValue = document.querySelector('.ano-novo .hours');
+    const minutesValue = document.querySelector('.ano-novo .minutes');
+    const secondsValue = document.querySelector('.ano-novo .seconds');
 
+    setInterval(() => {
+        daysValue.innerHTML = `${TempoAteAnoNovo.total.days}`;
+        hoursValue.innerHTML = `${TempoAteAnoNovo.total.hours}`;
+        minutesValue.innerHTML = `${TempoAteAnoNovo.total.minutes}`;
+        secondsValue.innerHTML = `${TempoAteAnoNovo.total.seconds}`;
+    }, 1000);
+}
 
+anoNovoCount();
+
+function carnavalCount() {
+    const daysValue = document.querySelector('.carnaval .days');
+    const hoursValue = document.querySelector('.carnaval .hours');
+    const minutesValue = document.querySelector('.carnaval .minutes');
+    const secondsValue = document.querySelector('.carnaval .seconds');
+
+    setInterval(() => {
+        daysValue.innerHTML = `${TempoAteCarnaval.total.days}`;
+        hoursValue.innerHTML = `${TempoAteCarnaval.total.hours}`;
+        minutesValue.innerHTML = `${TempoAteCarnaval.total.minutes}`;
+        secondsValue.innerHTML = `${TempoAteCarnaval.total.seconds}`;
+    }, 1000);
+}
+
+carnavalCount();
